81c2833816c77b36584e8275b9e330f147d4c903
Felix Niemeyer <niemeyer.felix@gmail.com>
2020-01-18 16:46:14 +0100

    degree distribution

---
+++ analysis/degree-distribution.py
@@ -0,0 +1,33 @@
+import collections
+import matplotlib.pyplot as plt 
+import numpy as np
+
+from network_loader import loadNetwork
+
+routes = loadNetwork()
+
+degree_count = {}
+max_degree = 0
+
+for n, d in routes.degree():
+	try:
+		degree_count[d] += 1
+	except:
+		degree_count[d] = 1
+	if d > max_degree: 
+		max_degree = d
+
+print(degree_count)
+
+degrees = np.empty(max_degree + 1)
+nodes = np.empty(max_degree + 1)
+
+for degree in range(0, max_degree + 1):
+	degrees[degree] = degree
+	try:
+		nodes[degree] = degree_count[degree]
+	except:
+		nodes[degree] = 0
+
+plt.plot(degrees, nodes)
+plt.show()
