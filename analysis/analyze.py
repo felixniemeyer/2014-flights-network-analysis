4f915e662dc7c6e7161d94942eaee5cfa65e82d3
Sai <saicharan9514@gmail.com>
2020-01-18 16:47:03 +0100

    linked the py with sqlite

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -2,14 +2,24 @@ import csv
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import sqlite3
 
 
 routes = nx.MultiDiGraph()
 
-with open('../data/routes.dat') as f: 
-    csv_reader = csv.reader(f)
-    for row in csv_reader: 
-        routes.add_edge(row[3], row[5]) 
+conn = sqlite3.connect('../fghjij.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM routes WHERE airline_id='4066'")
+
+rows = cursor.fetchall()
+for row in rows:
+    routes.add_edge(row[3], row[5])
+
+# with open('../data/routes.dat') as f:
+#     csv_reader = csv.reader(f)
+#     for row in csv_reader:
+#         routes.add_edge(row[3], row[5])
 
 nx.draw_networkx(routes)
 plt.show()
