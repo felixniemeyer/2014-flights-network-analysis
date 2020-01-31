20408e6724dac5be02fc3efb9bdcc05199df3906
Sai <saicharan9514@gmail.com>
2020-01-31 11:04:42 +0100

    analysis changes for presentation

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,5 +1,7 @@
+import networkx as nx
 import sqlite3
-
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 def sortedAirlines():
     conn = sqlite3.connect(
@@ -21,3 +23,18 @@ def sortedAirlines():
         new_airline_list.append([listItem[0], no_of_routes[0][0]])
 
     return sorted(new_airline_list, key=lambda x: x[1], reverse=True)
+
+
+def draw(G, pos, measures, measure_name):
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=250, cmap=plt.cm.plasma,
+                                   node_color=measures.values(),
+                                   nodelist=measures.keys())
+    nodes.set_norm(mcolors.SymLogNorm(linthresh=0.01, linscale=1))
+
+    # labels = nx.draw_networkx_labels(G, pos)
+    edges = nx.draw_networkx_edges(G, pos)
+
+    plt.title(measure_name)
+    plt.colorbar(nodes)
+    plt.axis('off')
+    plt.savefig('centrality.png')
--- analysis/main.py
+++ analysis/main.py
@@ -4,16 +4,17 @@ import analysis
 import matplotlib.pyplot as plt
 import scipy as sp
 
-
 # get a list of sorted airline IDs based on their frequency in the routes table
 sortedAirlines = analysis.sortedAirlines()
+airlines = []
 G = []
-for i in range(0, 1):
+for i in range(0, 7):
     conn = sqlite3.connect('/home/sai/Documents/WiSe 2019-2020/Network Economics/Case '
                            'Study/network-economics-case-study/db.db')
     cursor = conn.cursor()
     airlineId = str(sortedAirlines[i][0])
-    print(airlineId)
+    cursor.execute("SELECT name FROM airlines where airline_id=?", (airlineId,))
+    airlines.append(cursor.fetchone())
     cursor.execute("SELECT * FROM routes WHERE airline_id=?", (airlineId,))
     rows = cursor.fetchall()
     subG = nx.DiGraph()
@@ -22,35 +23,74 @@ for i in range(0, 1):
             subG.add_edge(row[3], row[5], weight=int(row[len(row) - 2]), flow=int(row[len(row) - 1]))
     G.append(subG)
 
+colors = ["g", "b", "r"]
+distGraph = []
+flowGraph = []
+centralityGraph = []
+details = []
+degreesList = []
+i = 0
 for subG in G:
-    print(subG.size())
-    print(subG.size(weight='weight'))
-    print(subG.size(weight='flow'))
-    adjacencyMatrix = nx.adjacency_matrix(subG)
-    #print(adjacencyMatrix)
+    dictDetails = {}
+    dictDetails["name"] = airlines[i]
+    # adjacencyMatrix = nx.adjacency_matrix(subG)
+    # print(adjacencyMatrix)
     distEdgeData = subG.edges.data('weight')
     sortedDistEdgeData = sorted(distEdgeData, key=lambda x: x[2], reverse=True)
-    print("dist diameter: ", sortedDistEdgeData[0])
+    listEdges = []
+    for edge in sortedDistEdgeData:
+        listEdges.append(edge[2])
+    distGraph.append(listEdges)
+    dictDetails["dist_diameter"] = sortedDistEdgeData[0]
+    dictDetails["totalDistance"] = subG.size(weight='weight')
+    dictDetails["avgDistance"] = subG.size(weight='weight') / len(sortedDistEdgeData)
     flowEdgeData = subG.edges.data('flow')
     sortedFlowEdgeData = sorted(flowEdgeData, key=lambda x: x[2], reverse=True)
-    print("flow diameter: ", sortedFlowEdgeData[0])
+    flowListEdgeData = []
+    for edge in sortedFlowEdgeData:
+        flowListEdgeData.append(edge[2])
+    flowGraph.append(flowListEdgeData)
+    dictDetails["flow_diameter"] = sortedFlowEdgeData[0]
+    dictDetails["totalFlow"] = subG.size(weight='flow')
+    dictDetails["avgFlow"] = subG.size(weight='flow') / len(flowListEdgeData)
     edgeData = subG.edges.data()
     listEdgeData = []
     for edges in edgeData:
-        listEdgeData.append([edges[0], edges[1], edges[2]['weight']*edges[2]['flow']])
+        listEdgeData.append([edges[0], edges[1], edges[2]['weight'] * edges[2]['flow']])
     sortedEdgeData = sorted(listEdgeData, key=lambda x: x[2], reverse=True)
-    print("net diameter: ", sortedEdgeData[0])
-    print(nx.average_shortest_path_length(subG))
-    dictCentrality = nx.closeness_centrality(subG)
+    dictDetails["net_diameter"] = sortedEdgeData[0]
+    dictDetails["avg_clustering_dist"] = nx.average_clustering(subG, weight='weight')
+    dictDetails["avg_clustering_flow"] = nx.average_clustering(subG, weight='flow')
+    #print(nx.average_shortest_path_length(subG))
+    details.append(dictDetails)
+    dictCentrality = nx.eigenvector_centrality(subG)
     listCentrality = []
     for key in dictCentrality:
         listCentrality.append([key, dictCentrality[key]])
     sortedCentrality = sorted(listCentrality, key=lambda x: x[1], reverse=True)
-    print("most prestigious node: ", sortedCentrality[0])
+    sortedListCentrality = []
+    for centrality in sortedCentrality:
+        sortedListCentrality.append(centrality[1])
+    centralityGraph.append(sortedListCentrality)
 
-    #nx.draw_networkx(subG, with_labels=True, node_size=10, font_size=2, arrowsize=4)
+    # nx.draw_networkx(subG, with_labels=True, node_size=10, font_size=2, arrowsize=4)
     degrees = [subG.degree(n) for n in subG.nodes()]
-    print(degrees)
-    print(nx.algorithms.distance_measures.diameter(subG, ))
-    #plt.hist(degrees, bins=50)
-    #plt.show()
+    degreesList.append(degrees)
+    i += 1
+    # plt.hist(degrees, bins=50)
+    # plt.show()
+with open('stats.txt', 'w') as f:
+    print(details, file=f)
+
+
+pos = nx.spring_layout(G[0])
+
+
+#analysis.draw(G[0], pos, nx.degree_centrality(G), 'Degree Centrality')
+plt.hist(degreesList[0], bins=100, label=airlines[0], color=colors[0])
+plt.hist(degreesList[1], bins=100, label=airlines[1], color=colors[1])
+plt.hist(degreesList[2], bins=100, label=airlines[2], color=colors[2])
+plt.xlabel("Degrees", fontsize=12)
+plt.ylabel("Frequency", fontsize=12)
+plt.legend()
+plt.savefig('hist.png')
